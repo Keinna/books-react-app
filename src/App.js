01e6b8aacc98fbbb4af02978ee5d1beebde93b89
Anniek <anniekgithub@gmail.com>
2023-05-22 21:29:16 +0200

    update code using axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import {useState} from 'react';
-import BookCreate from './components/BookCreate';
-import BookList from './components/BookList';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
-function App(){
+function App() {
     const [books, setBooks] = useState([]);
 
-    const deleteBookById = (id) => {
+    const fetchBooks = async () => {
+        const response = await axios.get("http://localhost:3001/books");
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
         });
@@ -13,37 +24,42 @@ function App(){
         setBooks(updatedBooks);
     };
 
-    const editBookByID = (id, newTitle) => {
+    const editBookByID = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle,
+        });
         const updatedBooks = books.map((book) => {
-            if(book.id === id){
-                return { ...book, title: newTitle};
+            if (book.id === id) {
+                return { ...book, ...response.data };
             }
             return book;
         });
 
         setBooks(updatedBooks);
-    }
+    };
 
-    const createBook = (title) => {
-        const updatedBooks = [
-            //go and find the existing books piece of state and copy them into new array
-            ...books,
-            {id: Math.round(Math.random() * 99999), title}
-        ]
-        setBooks(updatedBooks)
-        console.log(updatedBooks)
-    }
+    const createBook = async (title) => {
+        const response = await axios.post("http://localhost:3001/books", {
+            title,
+        });
+        console.log(response);
+        const updatedBooks = [...books, response.data];
+        setBooks(updatedBooks);
+    };
 
-return( 
-    <div className="app">
-      <div className="container-title">
-      <h1>Book Management App</h1>
-      </div>
-        <BookList books={books} onDelete={deleteBookById} onEdit={editBookByID}/>
-        <BookCreate onCreate={createBook} />
-      
-    </div>
-    )
+    return (
+        <div className="app">
+            <div className="container-title">
+                <h1>Book Management App</h1>
+            </div>
+            <BookList
+                books={books}
+                onDelete={deleteBookById}
+                onEdit={editBookByID}
+            />
+            <BookCreate onCreate={createBook} />
+        </div>
+    );
 }
 
 export default App;
